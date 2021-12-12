ca8c0354d6bb71dc1d69d61caa21b0f77b3d317d
Hershell Koger <hersheykoger@yahoo.com>
2021-12-12 14:53:43 -0500

    Add comments for first function computerPlay()

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world")
+
+//Create a function that will randomly return either "Rock", "Paper", "Scissors".
+//This function will provide the computer's play, so will be named computerPlay.
+//This function will generate a random number, then return one of the abover strings
+//depending on the number generated (i.e. 0-0.33 return "Rock", 0.34-0.66 return "Paper", etc.)
+
+
