e8aac090ff770caa63afb702d5920201dc0b3297
Hershell Koger <hersheykoger@yahoo.com>
2021-12-12 20:04:57 -0500

    Create playRound function, which uses conditional statements that address each outcome of a round ('Scissors beat Paper, Rock crushes Scissors, etc.)

--- script.js
+++ script.js
@@ -17,17 +17,39 @@ function computerPlay() {
     }
 }
 
+// console.log("Computer plays " + computerPlay());
+
 //Create a function that plays a single round of Rock-Paper-Scissor.
 //The function will take two parameters - one for the players selecetion,
 //and one for the computer's selection. It should return a string announcing a winner,
 //somthing like "You lose! Paper beats rock!" I plan to have three separate conditionals,
 //each addressing a particular outcome with a corresponding return string indicating 
-//who won.
+//who won. The playerSelection parameter needs to be case-insensitive.
+
+const playerSelection = "Scissors";
+
+const computerSelection = computerPlay();
 
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection == computerSelection) {
+        return "It's a tie!";
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You win! Rock crushes Scissors!"
+    } else if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You lose! Paper (somehow) smothers Rock!"
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You win! Scissors cut up Paper!"
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "You lose! Rock crushes scissors!"
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You lose! Scissors cut up Paper!"
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You win! Paper (somehow) smothers Rock"
+    }
 }
 
-console.log(computerPlay());
+
+
+console.log(playRound(playerSelection, computerSelection));
 
 
