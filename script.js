1bb01febdf2c409bd0b0758a2d76fdcc7af2af79
Hershell Koger <89715064+kogerhj@users.noreply.github.com>
2024-09-13 22:37:50 -0400

    Refactor and create game() function
    Created the game() function to loop the playRound()
    function several times. Moved playRound and score
    variablesl into the game function.

--- script.js
+++ script.js
@@ -1,54 +1,72 @@
 
+const promptMessage = "The computer wants to play! Please enter one of the following: rock, paper, or scissors"
+
+
 function getComputerChoice() {
-    let optionsArray = ["rock", "paper", "scissors"]; 
+    let optionsArray = ["rock", "paper", "scissors"];
     randNum = Math.floor(Math.floor(Math.random() * 3));
     return optionsArray[randNum];
 }
 
-console.log("Computer plays " + getComputerChoice());
+function getHumanChoice() {
+    let answer = prompt(promptMessage).toLowerCase();
+    return answer;
+}
 
-//This is the message users get when they open the webpage, prompting them to enter their play
-const promptMessage = "The computer wants to play! Please enter one of the following: rock, paper, or scissors"
-
-//This will tie the above promptMessage variable to the playerSelection variable, which will be used
-//in the playRound() function
-// let playerSelection = prompt(promptMessage);
-
-let playerSelection = "rock";
-
-//This will tie the computerPlay function above to the computerSelection parameter in the
-//playRound function
 const computerSelection = getComputerChoice();
 
-function playRound(playerSelection, computerSelection) {
+const playerSelection = getHumanChoice();
 
-    let newAnswer = playerSelection.toLowerCase();
 
-    if (newAnswer == computerSelection) {
-        return "It's a tie!";
-    } else if (newAnswer == "rock" && computerSelection == "scissors") {
-        return "You win! Rock crushes Scissors!"
-    } else if (newAnswer == "rock" && computerSelection == "paper") {
-        return "You lose! Paper (somehow) smothers Rock!"
-    } else if (newAnswer == "scissors" && computerSelection == "paper") {
-        return "You win! Scissors cut up Paper!"
-    } else if (newAnswer == "scissors" && computerSelection == "rock") {
-        return "You lose! Rock crushes scissors!"
-    } else if (newAnswer == "paper" && computerSelection == "scissors") {
-        return "You lose! Scissors cut up Paper!"
-    } else if (newAnswer == "paper" && computerSelection == "rock") {
-        return "You win! Paper (somehow) smothers Rock"
-    } else {
-        return "Sorry, that is not a valid answer - please check spelling and try again."
+
+
+function game() {
+
+    let humanScore = 0;
+
+    let computerScore = 0;
+
+    function playRound(playerSelection, computerSelection) {
+
+        if (playerSelection == computerSelection) {
+            return "It's a tie!";
+        } else if (playerSelection == "rock" && computerSelection == "scissors") {
+            humanScore++;
+            // return "You win! Rock crushes Scissors!"
+        } else if (playerSelection == "rock" && computerSelection == "paper") {
+            computerScore++;
+            // return "You lose! Paper (somehow) smothers Rock!"
+        } else if (playerSelection == "scissors" && computerSelection == "paper") {
+            humanScore++;
+            // return "You win! Scissors cut up Paper!"
+        } else if (playerSelection == "scissors" && computerSelection == "rock") {
+            computerScore++;
+            // return "You lose! Rock crushes scissors!"
+        } else if (playerSelection == "paper" && computerSelection == "scissors") {
+            computerScore++;
+            // return "You lose! Scissors cut up Paper!"
+        } else if (playerSelection == "paper" && computerSelection == "rock") {
+            humanScore++;
+            // return "You win! Paper (somehow) smothers Rock"
+        } else {
+            // return "Sorry, that is not a valid answer - please check spelling and try again."
+        }
+    }
+
+    for (let i = 0; i <= 1; i++) {
+        getComputerChoice();
+        getHumanChoice();
+
+        
+        playRound(playerSelection, computerSelection);
+        console.log(humanScore, computerScore);
     }
 }
 
-function game() {
-   for (let i = 1; i <= 5; i++) {
-      console.log(playRound(playerSelection, computerSelection))
-   }
-}
-   
+game();
+
+
+
 
 
 
